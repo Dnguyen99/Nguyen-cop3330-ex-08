8028600b3413be45e37520669f6685abef3671d0
Dnguyen99 <dylannguyen@Knights.ucf.edu>
2021-09-10 12:32:13 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+import java.util.*;
+import java.lang.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+
+        System.out.println("How many people? ");
+        int people = input.nextInt();
+        System.out.println("How many pizzas do you have? ");
+        int pizza = input.nextInt();
+        System.out.println("How many slices per pizza? ");
+        int slices = input.nextInt();
+        int total = slices * pizza;
+        System.out.println(people+ " people with " +pizza+ " pizzas ("+total+" slices)");
+
+        if (total / people == 1) {
+            System.out.println("Each person gets " +total / people+ " pieces of pizza.");
+            System.out.println("There are " +(total % people)+ "leftover pieces.");
+        }
+        else {
+            System.out.println("Each person gets " +total / people+ " pieces of pizza.");
+            System.out.println("There are " +(total % people)+ " leftover pieces.");
+        }
+    }
+}
